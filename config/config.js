3d09589b5e762e494e2bcbf822b94905f0448890
insidepixce <insidepixce@gmail.com>
2024-08-21 22:13:11 +0900

    Add config.js file

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    database: {
+        url: process.env.JAWSDB_URL || 'mysql://jhe7d7bdir90tmrv:egy7tty8vj4l5p6f@g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jsne5p21tvj8kt5x',
+        dialect: 'mysql',  // JawsDB는 MySQL 기반
+    }
+};
+
+export default config;
+
+
